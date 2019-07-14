9f0f456f862361574d41c84288899ae129f88156
Taucer <geoffreytaucer@gmail.com>
2019-07-14 10:55:10 -0400

    Initial commit

---
+++ calc.js
@@ -0,0 +1,13 @@
+function create_numbers() {
+    const number_div = document.querySelector('.numbers');
+
+    for (let i = 1; i < 10; i++) {
+        const button = document.createElement('button');
+        button.textContent = i;
+        number_div.appendChild(button);
+    }
+
+    const zero_button = document.createElement('button');
+    zero_button.style.columnSpan = 3
+    number_div.appendChild(zero_button);
+}
