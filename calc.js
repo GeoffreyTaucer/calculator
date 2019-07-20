7bb2c4c71ace66abb95dce2544af56e540e27577
Taucer <geoffreytaucer@gmail.com>
2019-07-20 17:04:04 -0400

    Fully functional for single-digit numbers, but divide by zero catch doesn't work for unknown reasons

--- calc.js
+++ calc.js
@@ -1,32 +1,68 @@
+let input_arr = [];
+let input_field = document.getElementById('input');
+let output_field = document.getElementById('output');
+let operations = ["*", "/", "+", "-"];
+
 function create_numbers() {
     const number_div = document.querySelector('.numbers');
 
     for (let i = 1; i < 10; i++) {
         const button = document.createElement('button');
         button.textContent = i;
+        button.onclick = function() {
+            update_input(i)
+        }
         number_div.appendChild(button);
     }
 
     const zero_button = document.createElement('button');
     zero_button.style.columnSpan = 2;
     zero_button.textContent = "0";
+    zero_button.onclick = function() {
+        update_input(0);
+    }
     number_div.appendChild(zero_button);
 
-    const decimal_button = document.createElement('button');
-    decimal_button.textContent = ".";
-    number_div.appendChild(decimal_button);
+    let operand_buttons = []
+
+    for (let i = 0; i < operations.length; i++){
+        let button = document.getElementById(operations[i])
+        button.onclick = function() {
+            update_input(operations[i])
+        }
+    }
+
+    equals_button = document.getElementById('=');
+    equals_button.onclick = equals;
+
+    clear_button = document.getElementById('C');
+    clear_button.onclick = clear;
 }
 
-function pemdas(in_string) {
-    let in_arr = in_string.split(' ');
-    let operations = ["*", "/", "+", "-"];
+function clear() {
+    input_arr = [];
+    out_arr = [];
+    input_field.textContent = "0"
+    output_field.textContent = "0"
+}
+
+function equals() {
+    pemdas(input_arr);
+}
+
+function update_input(input) {
+    input_arr[input_arr.length] = input;
+    input_field.textContent = input_arr.join('');
+}
+
+function pemdas(in_arr) {
     let out_arr;
 
-    for (i = 0; i < operations.length; i++) {
+    for (let i = 0; i < operations.length; i++) {
         out_arr = operate(in_arr, operations[i]);
     }
 
-    return out_arr[0];
+    output_field.textContent = String(out_arr[0]);
 }
 
 function operate(arr, operand) {
