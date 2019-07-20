68b4536482cc183ae99e5a7c68c24312e3ad013c
Taucer <geoffreytaucer@gmail.com>
2019-07-20 11:02:25 -0400

    Add catch for division by zero

--- calc.js
+++ calc.js
@@ -8,20 +8,25 @@ function create_numbers() {
     }
 
     const zero_button = document.createElement('button');
-    zero_button.style.columnSpan = 3;
+    zero_button.style.columnSpan = 2;
     zero_button.textContent = "0";
     number_div.appendChild(zero_button);
+
+    const decimal_button = document.createElement('button');
+    decimal_button.textContent = ".";
+    number_div.appendChild(decimal_button);
 }
 
 function pemdas(in_string) {
     let in_arr = in_string.split(' ');
     let operations = ["*", "/", "+", "-"];
+    let out_arr;
 
     for (i = 0; i < operations.length; i++) {
-        operate(in_arr, operations[i]);
+        out_arr = operate(in_arr, operations[i]);
     }
 
-    return in_arr[0];
+    return out_arr[0];
 }
 
 function operate(arr, operand) {
@@ -30,26 +35,38 @@ function operate(arr, operand) {
         indOper = arr.indexOf(operand);
         if (indOper === -1) {
             return arr
-        }
-        switch (operand) {
-            case "*":
-                arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) * parseInt(arr[indOper + 1]));
-                break;
+        } else {
+            switch (operand) {
+                case "*":
+                    arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) * parseInt(arr[indOper + 1]));
+                    break;
 
-            case "/":
-                arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) / parseInt(arr[indOper + 1]));
-                break;
+                case "/":
+                    if (arr[indOper + 1] === "0") {
+                        divide_by_zero()
+                        arr[0] = "YOU DONE FUCKED UP!"
+                        return arr;
+                    } else {
+                        arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) / parseInt(arr[indOper + 1]));
+                        break;
+                    }
 
-            case "+":
-                arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) + parseInt(arr[indOper + 1]));
-                break;
+                case "+":
+                    arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) + parseInt(arr[indOper + 1]));
+                    break;
 
-            case "-":
-                arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) - parseInt(arr[indOper + 1]));
+                case "-":
+                    arr.splice(indOper - 1, 3, parseInt(arr[indOper - 1]) - parseInt(arr[indOper + 1]));
+                    break;
+            }
         }
     }
 }
 
-console.log(pemdas("5 * 2 + 4 / 2 - 1"))
+function divide_by_zero() {
+    console.log("DIVIDED BY ZERO! APOCALYPSE IMMANENT!")
+}
 
-// create_numbers()
+console.log(pemdas("6 / 0"))
+
+create_numbers()
